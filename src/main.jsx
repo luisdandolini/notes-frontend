53736e92c1eafb6223d4cf7c947b4ba3a49a86f0
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-09-08 12:05:08 -0300

    feat: install styled components and created global theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { Details } from "./pages/Details";
+
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+
+export function Details() {
+  
+  return(
+    <Container>
+      <h1>App</h1>
+    </Container>
+  )
+}
