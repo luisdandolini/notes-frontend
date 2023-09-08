a9637a397a05df0c71b0ec5c0c1c3202903223a3
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-09-08 12:15:22 -0300

    feat: created global styled

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { Details } from "./pages/Details";
 
+import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Details />
+      <GlobalStyles />
+        <Details />
     </ThemeProvider>
   </React.StrictMode>,
 )
