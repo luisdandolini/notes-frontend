8f86d08734e22aecd2f6411b0eb1323862215549
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-09-15 13:48:28 -0300

    feat: created method login

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { Routes } from "./routes";
-
+import { AuthProvider } from './hooks/auth';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 
@@ -10,7 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-        <Routes />
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
