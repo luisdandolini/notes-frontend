7dbdd5b68c9dfc205d11eb0dd9a067dc07ea53d7
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-09-15 20:45:37 -0300

    feat: created method create new note

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
@@ -6,9 +7,65 @@ import { NoteItem } from '../../components/NoteItem';
 import { Section } from '../../components/Section';
 import { Button } from '../../components/Button';
 import { Container, Form } from './styles';
-
+import { api } from '../../services/api';
+import { useNavigate } from 'react-router-dom';
 
 export function New() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState("");
+
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleAddLink() {
+    setLinks(prevState => [...prevState, newLink]);
+    setNewLink("");
+  }
+
+  function handleRemoveLink(deleted) {
+    setLinks(prevState => prevState.filter(link => link !== deleted));
+    setNewLink("");
+  }
+
+  function handleAddTag() {
+    setTags(prevState => [...prevState, newTag]);
+    setNewTag("");
+  }
+
+  function handleRemoveTag(deleted) {
+    setTags(prevState => prevState.filter(tag => tag !== deleted));
+    setNewTag("");
+  }
+
+  async function handleNewNote(){
+    if(!title) {
+      return alert("Digite o título da nota!")
+    }
+
+    if(newLink) {
+      return alert("Você deixou um link no campo para adicionar, mas não adicionou!!!");
+    }
+
+    if(newTag) {
+      return alert("Você deixou uma tag no campo para adicionar, mas não adicionou!!!");
+    }
+
+    await api.post("/notes", {
+      title,
+      description,
+      tags,
+      links
+    });
+
+    alert("Nota cadastrada com sucesso!");
+    navigate("/");
+  }
+
   return(
     <Container>
       <Header />
@@ -22,37 +79,56 @@ export function New() {
 
           <Input 
             placeholder="Título"
+            onChange={e => setTitle(e.target.value)}
           />
 
           <Textarea 
             placeholder="Observações"
+            onChange={e => setDescription(e.target.value)}
           />
 
           <Section title="Links úteis">
-            <NoteItem 
-              value="http://rocketseat.com.br"
-            />
-
+            {
+              links.map((link, index) => (
+                <NoteItem 
+                  key={String(index)}
+                  value={link}
+                  onClick={() => handleRemoveLink(link)}
+                />
+              ))
+            }
             <NoteItem 
               isNew
               placeholder="Novo link"
+              value={newLink}
+              onChange={e => setNewLink(e.target.value)}
+              onClick={handleAddLink}
             />
           </Section>
 
           <Section title='Marcadores'>
             <div className='tags'>
-              <NoteItem 
-                value="react"
-              />
+              {
+                tags.map((tag, index) => (
+                  <NoteItem 
+                    key={String(index)}
+                    value={tag}
+                    onClick={() => handleRemoveTag(tag)}
+                  />
+                ))
+              }
 
               <NoteItem 
                 isNew
                 placeholder="Nova tag"
+                onChange={e => setNewTag(e.target.value)}
+                value={newTag}
+                onClick={handleAddTag}
               />
             </div>
           </Section>
 
-          <Button title="Salvar" />
+          <Button title="Salvar" onClick={handleNewNote} />
         </Form>
       </main>
     </Container>
