b90b633cefdd96dbfe92cfbf9285bdebe1a9e27b
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-09-15 23:22:21 -0300

    fix: updated photo profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -21,14 +21,16 @@ export function Profile() {
   const [avatarFile, setAvatarFile] = useState(null);
 
   async function handleUpted() {
-    const user = {
+    const updated = {
       name,
       email,
       password: passwordNew,
       old_password: passwordOld
     }
 
-    await updateProfile({ user, avatarFile });
+    const userUpdated = Object.assign(user, updated);
+
+    await updateProfile({ user: userUpdated, avatarFile });
   }
 
   function handleChangeAvatar(event) {
